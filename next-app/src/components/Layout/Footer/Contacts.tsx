fd0e28baf29414151f2af271c58f78cb1980d9ca
ArekKuna <arkadiusz.kuna@jutromedical.com>
2023-04-16 00:24:55 +0200

    feat: add footer to global layut component

---
+++ next-app/src/components/Layout/Footer/Contacts.tsx
@@ -0,0 +1,10 @@
+export const Contacts = () => {
+  return (
+    <div className="col-span-4 flex justify-around text-xs font-light">
+      <a href="tel:+48577162333 ">+48 577 162 333</a>
+      <a href="mailto:aniakolatorskafoto@gmail.com">
+        aniakolatorskafoto@gmail.com
+      </a>
+    </div>
+  );
+};
