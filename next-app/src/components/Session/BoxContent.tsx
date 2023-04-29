5e63b324ed4652d7a847c0eed30ee6fd611b1972
ArekKuna <arkadiusz.kuna@jutromedical.com>
2023-04-29 20:50:18 +0200

    feat(session-page): modify boxContent component to render html from strapi richtext field

--- next-app/src/components/Session/BoxContent.tsx
+++ next-app/src/components/Session/BoxContent.tsx
@@ -3,13 +3,17 @@ type BoxContentProps = {
   description: string;
 };
 
+const createMarkup = (description: string) => {
+  return { __html: description };
+};
+
 export const BoxContent = ({ title, description }: BoxContentProps) => {
   return (
     <div className="space-y-6">
       <h1 className="font-signRiyathi text-4xl capitalize text-center">
         {title}
       </h1>
-      <p>{description}</p>
+      <div dangerouslySetInnerHTML={createMarkup(description)}></div>
     </div>
   );
 };
