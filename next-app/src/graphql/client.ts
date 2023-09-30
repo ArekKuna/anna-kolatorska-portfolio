501320ab4fcdf2d39d52ed6607de877a4505c32b
ArekKuna <arkadiusz.kuna@jutromedical.com>
2023-09-30 14:03:16 +0200

    add settings adjustemnts to run project

--- next-app/src/graphql/client.ts
+++ next-app/src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./generated";
 
-const client = new GraphQLClient("http://localhost:1337/graphql");
+const client = new GraphQLClient("http://127.0.0.1:1337/graphql");
 
 export const sdk = getSdk(client);
