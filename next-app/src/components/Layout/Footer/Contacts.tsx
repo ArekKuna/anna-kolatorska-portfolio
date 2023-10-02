b75d46ef1daee05a09bf287c2ff24ab99d3936ec
ArekKuna <arkadiusz.kuna@jutromedical.com>
2023-10-02 09:58:44 +0200

    feat(session-page): add contacts underline

--- next-app/src/components/Layout/Footer/Contacts.tsx
+++ next-app/src/components/Layout/Footer/Contacts.tsx
@@ -1,6 +1,6 @@
 export const Contacts = () => {
   return (
-    <div className="flex justify-center items-center gap-5 text-xs font-medium xl:text-base xl:gap-10">
+    <div className="flex justify-center items-center gap-5 text-xs font-medium underline underline-offset-4 xl:text-base xl:gap-10">
       <a href="tel:+48577162333 ">+48 577 162 333</a>
       <a href="mailto:aniakolatorskafoto@gmail.com">
         aniakolatorskafoto@gmail.com
