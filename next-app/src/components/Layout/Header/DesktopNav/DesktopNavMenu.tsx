22961d24c83f58cfe28ae46f8212aaf025672558
ArekKuna <arkadiusz.kuna@jutromedical.com>
2024-03-15 20:38:38 +0100

    feat(newHeader): fix category links in desktop header

--- next-app/src/components/Layout/Header/DesktopNav/DesktopNavMenu.tsx
+++ next-app/src/components/Layout/Header/DesktopNav/DesktopNavMenu.tsx
@@ -3,12 +3,11 @@ import { useHeader } from "components/Layout/Header/useHeader";
 
 export const DesktopNavMenu = () => {
   const { navigationMap } = useHeader();
+
   return (
     <ul className="flex items-center gap-4 list-none text-base font-normal uppercase">
       {navigationMap.map((link) => (
-        <li key={link.id} className="relative items-center">
-          <DesktopNavMenuItem links={link} />
-        </li>
+        <DesktopNavMenuItem link={link} />
       ))}
     </ul>
   );
--- next-app/src/components/Layout/Header/DesktopNav/DesktopNavMenuItem.tsx
+++ next-app/src/components/Layout/Header/DesktopNav/DesktopNavMenuItem.tsx
@@ -1,75 +1,28 @@
-import { DesktopNavCategoryItem } from "components/Layout/Header/DesktopNav/DesktopNavCategoryItem";
-import { NavigationMenu } from "components/Layout/Header/headerConfig";
-import { useScroll } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { NavigationMenu } from "components/Layout/Header/useHeader";
 
 type DesktopNavMenuItemProps = {
-  links: NavigationMenu;
+  link: NavigationMenu;
 };
 
-export const DesktopNavMenuItem = ({ links }: DesktopNavMenuItemProps) => {
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
-
+export const DesktopNavMenuItem = ({ link }: DesktopNavMenuItemProps) => {
   const router = useRouter();
   const path = router.pathname;
-  const activePath = path === links.href;
-
-  const { scrollY } = useScroll();
-
-  const updateSubmenuOpenState = () => {
-    const current = scrollY.get();
-    const prev = scrollY.getPrevious();
-
-    if (!prev) return 0;
-
-    if (current > 100 && current > prev) {
-      setIsSubmenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    setIsSubmenuOpen(false);
-    return scrollY.on("change", () => updateSubmenuOpenState());
-  }, [path]);
+  const activePath = path === link.href;
 
   return (
-    <>
-      {links.href ? (
-        <Link
-          href={links.href}
-          className={`${
-            activePath
-              ? "font-semibold hover:opacity-50"
-              : "font-light hover:opacity-50"
-          }`}
-        >
-          {links.text}
-        </Link>
+    <li
+      key={link.id}
+      className={`hover:opacity-50 cursor-pointer ${
+        activePath ? "font-bold" : "font-medium"
+      }`}
+    >
+      {link.href ? (
+        <Link href={link.href ?? ""}>{link.text}</Link>
       ) : (
-        <span
-          className="cursor-pointer hover:opacity-50"
-          onClick={() => setIsSubmenuOpen((prev) => !prev)}
-        >
-          {links.text}
-        </span>
+        <>{link.text}</>
       )}
-      {links.categories && (
-        <ul
-          className={`absolute w-[200px] overflow-hidden text-xs rounded-xl shadow-lg divide-y-[1px] divide-black/30 duration-500 ${
-            isSubmenuOpen ? "max-h-[200px] pt-[10px]" : "max-h-0"
-          } bg-white/90`}
-        >
-          {links.categories?.map((category) => (
-            <DesktopNavCategoryItem
-              key={category.text}
-              categories={category}
-              onClick={() => setIsSubmenuOpen((prev) => !prev)}
-            />
-          ))}
-        </ul>
-      )}
-    </>
+    </li>
   );
 };
